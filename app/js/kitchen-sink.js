c8f88e8584f1ad35fa35f5dbdaec6292fb1ad7a5
wk633 <wk633@outlook.com>
2017-06-29 21:38:06 -0700

    complete index and simple login

--- app/js/kitchen-sink.js
+++ app/js/kitchen-sink.js
@@ -11,7 +11,8 @@ var $$ = Dom7;
 // Add main view
 var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
+    dynamicNavbar: true,
+    domCache: true
 });
 // 1 Slide Per View, 50px Between
 var mySwiper1 = myApp.swiper('.new-startups-slider', {
@@ -41,3 +42,31 @@ myApp.onPageInit('startup', function(page) {
 	   myApp.alert('Our experts shall review your application and connect with you in 3-4 business days.', 'Thank You!');
 	});
 });
+
+
+$$('#login-btn').on('click', function(){
+    var data = myApp.formToJSON('#login-form');
+    data.login = true;
+    var url = "validation.php";
+    console.log(data)
+    
+    $$.ajax({
+        url: url,
+        method: 'POST',
+        data: data,
+        dataType: 'json',
+        success: loginCallBack,
+        cache: false  
+    })
+})
+
+var loginCallBack = function(e){
+    console.log("loginCallback");
+    console.log(e);
+    if (e.status == 'success') {
+        console.log("validation");
+        mainView.router.load({pageName: 'home'});
+    }else {
+        alert("wrong password or invalid username");
+    }
+}
